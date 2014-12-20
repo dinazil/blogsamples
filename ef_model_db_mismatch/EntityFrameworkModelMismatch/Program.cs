867be2b1a4b479f78f60d39b7fd409cd3b7e3305
Dina Goldshtein <dinazil@gmail.com>
2014-12-20 16:48:46 +0200

    minor
    new query after the generated key changed

--- ef_model_db_mismatch/EntityFrameworkModelMismatch/Program.cs
+++ ef_model_db_mismatch/EntityFrameworkModelMismatch/Program.cs
@@ -16,10 +16,11 @@ namespace EntityFrameworkModelMismatch
                 Console.WriteLine("Total number of rows in data model: {0}", ctx.Data.Count());
 
                 var query = from d in ctx.Data
-                            where d.Id == new Guid("2cc3c76e-854d-4c6e-b7ad-23cdc222de1b") && d.Timestamp < new DateTime(2014, 11, 28, 17, 0, 0)
+                            where d.Id == new Guid("82295fa4-e5e0-4152-a5e7-0c06e25f803a") 
+                            && d.Timestamp < new DateTime(2014, 12, 2, 19, 0, 0)
                             select d;
 
-                foreach (var d in query.ToList())
+                foreach (var d in query)
                 {
                     Console.WriteLine(d);
                 }
