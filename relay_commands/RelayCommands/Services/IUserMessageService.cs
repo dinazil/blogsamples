0686edd692899913cd58ccabe164af566d2b0033
Dina Goldshtein <dinazil@gmail.com>
2015-01-16 12:10:12 +0200

    added use message interface and real implementation

---
+++ relay_commands/RelayCommands/Services/IUserMessageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelayCommands.Services
+{
+    interface IUserMessageService
+    {
+        void DisplayMessage(string title, string content);
+    }
+}
