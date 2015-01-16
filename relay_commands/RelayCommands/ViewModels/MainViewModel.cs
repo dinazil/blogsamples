f973c683f36e729934e06c9364527e5b2ca77ac7
Dina Goldshtein <dinazil@gmail.com>
2015-01-16 12:25:03 +0200

    added mainviewmodel and viewmodellocator

---
+++ relay_commands/RelayCommands/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using RelayCommands.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelayCommands.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        private readonly IUserMessageService _userMessageService;
+        public MainViewModel(IUserMessageService userMessageService)
+        {
+            _userMessageService = userMessageService;
+
+            Title = "Relay Commands Demo";
+        }
+
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set { Set(() => Title, ref _title, value); }
+        }
+    }
+}
---
+++ relay_commands/RelayCommands/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using RelayCommands.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelayCommands.ViewModels
+{
+    class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<IUserMessageService, UserMessageService>();
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel MainViewModel
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+    }
+}
