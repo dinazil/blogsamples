84f84625db2c2c96e2003778c61f3427aa1301ab
Dina Goldshtein <dinazil@gmail.com>
2015-03-16 20:31:12 +0200

    added test for MSTest

---
+++ unit_test_initialization/UnitTestInitialization/MSTestTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestInitialization
+{
+    [TestClass]
+    public class MSTestTest
+    {
+        private int _instanceCounter = 0;
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            ++_instanceCounter;
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual(1, _instanceCounter);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual(2, _instanceCounter);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual(3, _instanceCounter);
+        }
+    }
+}
