549c3b5005f5cf166e950f3053a7d040610aaf04
Dina Goldshtein <dinazil@gmail.com>
2015-03-26 13:23:50 +0200

    added xUnit tests
    installed xUnit package and test runners

---
+++ unit_test_initialization/UnitTestInitialization/xUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTestInitialization
+{
+    public class xUnitTest
+    {
+        private int _instanceCounter = 0;
+        private static int _classCounter = 0;
+
+        public xUnitTest()
+        {
+            ++_instanceCounter;
+            ++_classCounter;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, _instanceCounter);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(2, _instanceCounter);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal(3, _instanceCounter);
+        }
+
+        [Fact]
+        public void StaticTest4()
+        {
+            Assert.Equal(4, _classCounter);
+        }
+
+        [Fact]
+        public void StaticTest5()
+        {
+            Assert.Equal(5, _classCounter);
+        }
+
+        [Fact]
+        public void StaticTest6()
+        {
+            Assert.Equal(6, _classCounter);
+        }
+    }
+}
