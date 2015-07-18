3448010466a275e79b326fac98864cbe449f4869
Dina Goldshtein <dinazil@gmail.com>
2015-07-18 19:22:14 +0300

    initial implementation - the wrapper code except the methods themselves

--- run_time_code_generation/RpcClientGenerator/ClientGenerator.cs
+++ run_time_code_generation/RpcClientGenerator/ClientGenerator.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace RpcClientGenerator
 {
 	public static class ClientGenerator
 	{
-		public static T GenerateRpcClient<T> () where T : class
+		public static T GenerateRpcClient<T> (TimeSpan timeout) where T : class
 		{
 			throw new NotImplementedException ();
 		}
@@ -15,9 +16,36 @@ namespace RpcClientGenerator
 			throw new NotImplementedException ();
 		}
 
+		private static string GeneratePropertiesCode()
+		{
+			return @"public TimeSpan Timeout { get; set; }";
+		}
+
+		private static string GeneratePrefixCode<T>()
+		{
+			string interfaceName = typeof(T).Name;
+			return @"
+					public class Client : ${interfaceName}
+					{
+					";
+		}
+
+		private static string GenerateSuffixCode()
+		{
+			return @"
+					}";
+		}
+
 		private static string GenerateInterfaceWrapperCode<T>()
 		{
-			throw new NotImplementedException ();
+			string start = GeneratePrefixCode<T> ();
+			string properties = GeneratePropertiesCode ();
+			string end = GenerateSuffixCode ();
+
+			var methodInfos = typeof(T).GetMethods (BindingFlags.Public | BindingFlags.Instance);
+			string methods = string.Join(Environment.NewLine, methodInfos.Select(GenerateMethodCode));
+
+			return "${start} ${properties} ${methods} ${end}";
 		}
 	}
 }
