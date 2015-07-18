5a8bb2c7a8cb92ea2989075266be6938829ba7bb
Dina Goldshtein <dinazil@gmail.com>
2015-07-18 19:01:21 +0300

    added skeleton for client generator

---
+++ run_time_code_generation/RpcClientGenerator/ClientGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace RpcClientGenerator
+{
+	public static class ClientGenerator
+	{
+		public static T GenerateRpcClient<T> () where T : class
+		{
+			throw new NotImplementedException ();
+		}
+
+		private static string GenerateMethodCode(MethodInfo method)
+		{
+			throw new NotImplementedException ();
+		}
+
+		private static string GenerateInterfaceWrapperCode<T>()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
