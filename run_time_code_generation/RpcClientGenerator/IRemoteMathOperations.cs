8e7caceeed66253ed9ae7650de1eb29aeb725174
Dina Goldshtein <dinazil@gmail.com>
2015-07-18 18:48:24 +0300

    added interface which we'd like to implement automatically

---
+++ run_time_code_generation/RpcClientGenerator/IRemoteMathOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RpcClientGenerator
+{
+	public interface IRemoteMathOperations
+	{
+		int Increment(int n);
+		int Decrement(int n);
+		double SquareRoot(double x);
+	}
+}
+
