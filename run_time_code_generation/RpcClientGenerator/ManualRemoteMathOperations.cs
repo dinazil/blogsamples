093156fd12dd639450e12131d5c3c7ce9542e1dd
Dina Goldshtein <dinazil@gmail.com>
2015-07-20 19:35:48 +0300

    added the file

---
+++ run_time_code_generation/RpcClientGenerator/ManualRemoteMathOperations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RpcClientGenerator
+{
+	public class ManualRemoteMathOperations : IRemoteMathOperations
+	{
+		private IRpcClient _client;
+
+		public ManualRemoteMathOperations(IRpcClient client)
+		{
+			_client = client;
+		}
+
+		public TimeSpan Timeout
+		{
+			get { return _client.Timeout; }
+			set 
+			{
+				_client.Timeout = value;
+			}
+		}
+
+		public int Increment(int n)
+		{
+			return (int)_client.ExecuteMethod ("Increment", n);
+		}
+
+		public int Decrement(int n)
+		{
+			return (int)_client.ExecuteMethod ("Decrement", n);
+		}
+
+		public double SquareRoot(double x)
+		{
+			return (double)_client.ExecuteMethod ("SquareRoot", x);
+		}
+	}
+}
+
