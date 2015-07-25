242e6f8fe4de58d3ab8aad19634318355bd0dba6
Dina Goldshtein <dinazil@gmail.com>
2015-07-25 13:45:33 +0300

    turns out according to https://erictummers.wordpress.com/2012/04/03/basichttpbinding-blocking-under-mono/ that in Mono we can't have timeout for BasicHttpBinding. So use NetTcp instead.

--- run_time_code_generation/MathOperationsServiceHost/Program.cs
+++ run_time_code_generation/MathOperationsServiceHost/Program.cs
@@ -28,6 +28,9 @@ namespace MathOperationsServiceHost
 				host.AddServiceEndpoint (typeof(IMathOperationsService),
 					new BasicHttpBinding (), "");
 
+				host.AddServiceEndpoint (typeof(IMathOperationsService),
+					new NetTcpBinding (), "net.tcp://localhost:12346/MathOperations");
+
 				host.Open();
 
 				Console.WriteLine("The service is ready at...");
