a066bb5cecc8ce705714ca592a9f5fdf6ed6170d
Dina Goldshtein <dinazil@gmail.com>
2015-07-25 13:46:10 +0300

    connect to the NetTcp endpoint and run some operations that throw exceptions.

--- run_time_code_generation/MathRunner/Program.cs
+++ run_time_code_generation/MathRunner/Program.cs
@@ -7,14 +7,41 @@ namespace MathRunner
 	{
 		public static void Main (string[] args)
 		{
-			string baseAddress = "http://localhost:12345/MathOperations";
-			using (var client = new MathOperationsServiceClient(new BasicHttpBinding(), new EndpointAddress(baseAddress)))
+			string baseAddress = "net.tcp://localhost:12346/MathOperations";
+			using (var client = new MathOperationsServiceClient(
+				new NetTcpBinding(), 
+				new EndpointAddress(baseAddress)))
 			{
+				client.InnerChannel.OperationTimeout = TimeSpan.FromSeconds (2);
+
 				Console.WriteLine ("Click Enter to start...");
 				Console.ReadLine ();
 				Console.WriteLine (client.Increment (0));
 				Console.WriteLine (client.Decrement (3));
 				Console.WriteLine (client.SquareRoot (9));
+
+				try
+				{
+					Console.WriteLine (client.SquareRoot (-16));
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine (ex.Message); 
+				}
+
+				Console.WriteLine (client.SquareRoot (25));
+
+				try
+				{
+					client.Timeout(TimeSpan.FromSeconds(5));
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine (ex.Message); 
+				}
+
+				Console.WriteLine (client.SquareRoot (36));
+
 			}
 		}
 	}
