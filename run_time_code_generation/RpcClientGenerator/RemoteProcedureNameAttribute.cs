08188b19ad96795f89fffe0ee8190d9d4354b1b5
Dina Goldshtein <dinazil@gmail.com>
2015-07-25 10:53:32 +0300

    added custom attribute for changing the name of the remote function

---
+++ run_time_code_generation/RpcClientGenerator/RemoteProcedureNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RpcClientGenerator
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public class RemoteProcedureNameAttribute : Attribute
+	{
+		public string Name { get; set;}
+		public RemoteProcedureNameAttribute (string name)
+		{
+			Name = name;
+		}
+	}
+}
+	
