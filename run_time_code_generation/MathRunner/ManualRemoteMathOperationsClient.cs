c05bd467cafc0b9702f83bf3a769847d985fe0e3
Dina Goldshtein <dinazil@gmail.com>
2015-07-26 18:33:05 +0300

    started writing smart client manually - to be later used as template for code generation

---
+++ run_time_code_generation/MathRunner/ManualRemoteMathOperationsClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace MathRunner
+{
+	public class ManualRemoteMathOperationsClient : ClientBase<IMathOperationsService>, IMathOperationsService
+	{
+		public delegate MathOperationsServiceClient ClientGenerator();
+
+		private ClientGenerator _clientGenerator;
+		private MathOperationsServiceClient _client;
+
+		public ManualRemoteMathOperationsClient (ClientGenerator clientGenerator)
+		{
+			_clientGenerator = clientGenerator;
+			_client = clientGenerator ();
+		}
+
+		private void RegenerateClientIfNeeded()
+		{
+			if (_client == null || _client.State == CommunicationState.Faulted) 
+			{
+				_client = _clientGenerator ();
+			}
+		}
+
+		public int Increment(int n) 
+		{
+			try
+			{
+				return _client.Increment(n);
+			}
+			finally 
+			{
+				RegenerateClientIfNeeded ();
+			}
+		}
+
+		public int Decrement(int n) 
+		{
+			try
+			{
+				return _client.Decrement(n);
+			}
+			finally 
+			{
+				RegenerateClientIfNeeded ();
+			}
+		}
+
+		public double SquareRoot(double x) 
+		{
+			try
+			{
+				return _client.SquareRoot(x);
+			}
+			finally 
+			{
+				RegenerateClientIfNeeded ();
+			}
+		}
+
+		public void Timeout(System.TimeSpan time) 
+		{
+			try
+			{
+				_client.Timeout(time);
+			}
+			finally 
+			{
+				RegenerateClientIfNeeded ();
+			}
+		}
+	}
+}
+
